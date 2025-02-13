60f73e9200a8c12c837ce43382d7dc2eac3f2b4f
KirillLin <kirilllinkevich005@gmail.com>
2025-02-13 21:12:19 +0300

    initial commit

---
+++ src/test/java/org/example/trainschedule/TrainScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.trainschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
